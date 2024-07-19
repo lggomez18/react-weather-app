55b474ecc9777912936dd69d407f7ffd94b35abc
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-18 20:15:00 -0600

    Installing and testing bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
-        Open-sourced on <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >Github</a>
+        Open-sourced on {""} <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >Github</a>
       </footer>
     </div>
   );
