df785e4a85888524865a50f5b284ef0e38bcb1a7
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-18 19:23:55 -0600

    Adding direct link to official website hosted on netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        Open-sourced on <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >Github</a>
+      </footer>
     </div>
   );
 }
