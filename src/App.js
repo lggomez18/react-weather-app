1c2d8571dedfc57a11dd7784e3ac1895ba1c4a07
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-18 19:28:34 -0600

    Changed to h1 for the main title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         Open-sourced on <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >Github</a>
       </footer>
