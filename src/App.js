d5d95ed686607a2398937de1ead027ea28617ccf
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-18 20:31:17 -0600

    Created a new component called weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="contaniner">
-
       <h1>Weather App</h1>
+      <Weather />
       <button className="btn btn-primary">Hello</button>
       <footer>
         Open-sourced on {""} <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >Github</a>
