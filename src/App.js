a50f0c136181985348ceec4c56e3c0aeb7e92dbb
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-24 13:33:47 -0600

    API in progress
    Continue to debug API calls and replacing the set states as seen in the code. Error shows temperature is not being defined.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="New York"/>
       <footer>
         This project was coded by Leticia Gmx and is{""} <a href="https://github.com/lggomez18/react-weather-app" target="_blank" rel="noreferrer" >open-sourced on Github</a>
       </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -2,22 +2,33 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather(){
-    const [load, setLoad]= useState(false);
-    const [temperature, setTemperature]= useState(null);
-    const [weatherData, setWeatherData]= useState({});
+export default function Weather(props){
+    const [weatherData, setWeatherData]= useState({ ready:false });
+    //const [city, setCity] = useState("London");
+    //const [ready, setReady]=useState(false);
+    //const [temperature, setTemperature]= useState(null);
+
     function handleResponse(response){
-        console.log(response.data);
+
+
         setWeatherData({
+            ready:true,
             temperature:response.data.main.temp,
-            wind: response.data.main.wind.speed,
+            wind: response.data.main.wind,
+            humidity:response.data.main.humidity,
+            date:"Wednesday 07:00",
+            description:response.data.weather[0].description,
+            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/sunny.png",
             city: response.data.name,
         });
-        setTemperature(response.data.main.temp);
-        setLoad(true);
+
+        //setTemperature(response.data.main.temp); instead of caling setTemperature, added inside setWeatherData
+        //setReady(true); instead of calling set ready, added inside setWeatherData
+
+
     }
 
-    if (load){
+    if (weatherData.ready){
         return (
         <div className="Weather">
         <form>
@@ -40,16 +51,18 @@ export default function Weather(){
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-            <li>Wednesday 07:00</li>
-            <li>{weatherData.description}</li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearfix">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Mostly cloudy" className="float-left"/>
+                    <img 
+                    src={weatherData.iconUrl} 
+                    alt={weatherData.description} className="float-left"/>
                   
                         <div className="float-left">
-                        <span className="temperature">{Math.round(temperature)}</span>
+                        <span className="temperature">{Math.round(weatherData.temperature)}</span>
                         <span className="unit">°C</span>
                         </div>
                     </div>
@@ -61,10 +74,10 @@ export default function Weather(){
                          Precipitation: 15%   
                         </li>
                         <li>
-                         Humidity: 30%
+                         Humidity:{weatherData.humidity}
                         </li>
                         <li>
-                         Wind: {weatherData.wind}
+                         Wind:{weatherData.wind}
                         </li>
                     </ul>
                 </div>
@@ -73,8 +86,7 @@ export default function Weather(){
         );
     } else {
         const apiKey ="290724cd93ad94b31t54c30cca2o800f";
-        let city = "New York";
-        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultcity}&key=${apiKey}&units=metric`;
     
         axios.get(apiUrl).then(handleResponse);
 
