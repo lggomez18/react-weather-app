aa21cdd5dc7787fce5d872dabfe9e705a3e74c78
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-08-03 09:07:19 -0600

    Building weekly forecast. In progress.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,19 +2,26 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+
+
+export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">
+        <div className="col-md-4">
           <div className="WeatherForecast-day">Thu</div>
           <WeatherIcon code="01d" size={36} />
+          
+          <div className="WeatherForecast-day">Thu</div>
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
           </div>
+
+            
         </div>
       </div>
     </div>
   );
 }
+
