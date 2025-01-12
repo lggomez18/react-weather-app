b345b31be4f439315751c2c13206949eba9e4616
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2025-01-12 12:05:23 -0600

    Weather Forecast component API in progress

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 
 
 export default function WeatherForecast(props) {
+  function handleResponse (response){
+    
+  }
+  
   const apiKey = "290724cd93ad94b31t54c30cca2o800f";
-let apiUrl =`https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apikey}`
+  let longitude = 40.7;
+  let latitude = 74;
+  let apiUrl =`https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apikey}`
+  
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
